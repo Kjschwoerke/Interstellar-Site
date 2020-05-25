639c61a77e02428affc727b761390d9cfb1ae50f
kjschwoerke <kjschwoerke@yahoo.com>
2020-05-24 19:01:20 -0500

    added animated rocket ship

---
+++ script.js
@@ -0,0 +1,13 @@
+const spaceShip = document.getElementById('ship-animated')
+
+function flyingShip() {
+    document.getElementById('ship-animated').style.animation = "ship-flying 1s linear forwards"  
+}
+
+flyingShip()
+
+
+
+
+
+
