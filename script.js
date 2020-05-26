f438e3bb8826a696ce8efeb7c3344088fb7f6404
kjschwoerke <kjschwoerke@yahoo.com>
2020-05-25 21:26:33 -0500

    added animation play on link to new page

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-const spaceShip = document.getElementById('ship-animated')
-spaceShip.style.zIndex = "1"
+// const spaceShip = document.getElementById('ship-animated')
+// spaceShip.style.zIndex = "1"
 
-function flyingShip() {
-    spaceShip.style.animation = "ship-flying 10s linear forwards"  
+// function flyingShip() {
+//     spaceShip.style.animation = "ship-flying 10s linear forwards"  
+// }
+
+// flyingShip()
+
+//Above code works!!!
+
+
+
+const spaceShip = document.getElementById('ship-animated')
+
+const transAnimation = () => {
+  spaceShip.style.animation = 'ship-flying 3s linear forwards'
 }
 
-flyingShip()
-
-
-
-
-
-
+$('#ship-animated').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', 
+function() {
+    //window.open('https://kjschwoerke.com/')
+    window.location.href = 'https://kjschwoerke.com/' 
+});
