910ee2f4b3162bbcc1224bc79410d53d1e4f342d
kjschwoerke <kjschwoerke@yahoo.com>
2020-06-01 17:35:12 -0500

    added slogan to link animation

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-// const spaceShip = document.getElementById('ship-animated')
-// spaceShip.style.zIndex = "1"
-
-// function flyingShip() {
-//     spaceShip.style.animation = "ship-flying 10s linear forwards"  
-// }
-
-// flyingShip()
-
-//Above code works!!!
-
-
 
 const spaceShip = document.getElementById('ship-animated')
+const slogan = document.getElementById("slogan")
 
 const transAnimation = () => {
-  spaceShip.style.animation = 'ship-flying 3s linear forwards'
+  spaceShip.style.animation = 'ship-flying 5s linear forwards'
+  slogan.style.animation = 'reveal 5s linear forwards'
 }
 
 $('#ship-animated').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', 
